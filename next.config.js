239ca40d2231d9f067319bbf10db539a26c7a8da
Jibin <jibin_1@hotmail.com>
2024-10-23 20:10:50 +0530

    workflow changes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/headless-cms-nextjs",
   output: "export",  
+  distDir: "dist",
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
+    unoptimized: true,
     domains: ["images.ctfassets.net"],
   },
 }
